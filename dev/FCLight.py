3d19d06d8d1226b65a62474e5467739d107e40a5
zhouxi's_pi <zhouxi121@live.com>
2017-10-16 19:16:05 +0800

    add light

---
+++ dev/FCLight.py
@@ -0,0 +1,70 @@
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Cannot import RPi.GPIO")
+import asyncio
+
+fc_light_gpio_channel = 18  # gpio1
+
+fc_light_fresh_rate = 0.5  # sec
+
+
+class FCLight(object):
+
+    # high status
+    FCLightHigh = False
+
+    # low status
+    FCLightLow = True
+
+    fc_light_now_status = FCLightLow
+
+    is_stop = False
+
+    # return now status
+    def get_light_status(self):
+        return self.fc_light_now_status
+
+    _instance = None
+
+    def __init__(self):
+        print("init FC Light")
+
+    # singleton
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(FCLight, cls).__new__(cls)
+        return cls._instance
+
+    def init_gpio(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(fc_light_gpio_channel, GPIO.IN)
+
+    def clean_gpio(self):
+        GPIO.cleanup(fc_light_gpio_channel)
+
+    @asyncio.coroutine
+    def start_service(self):
+        if not self.is_stop:
+            self.init_gpio()
+            while not self.is_stop:
+                yield from asyncio.sleep(fc_light_fresh_rate)
+                yield from self.update_light_status()
+
+            self.clean_gpio()
+
+    @asyncio.coroutine
+    def update_light_status(self):
+        status_tmp = GPIO.input(fc_light_gpio_channel)
+        if status_tmp != self.fc_light_now_status:
+            self.fc_light_now_status = status_tmp
+            status_name = "High"
+            if self.fc_light_now_status == FCLight.FCLightLow:
+                status_name = "Low"
+            print("FC Light new status :" + status_name)
+
+    def stop_service(self):
+        self.is_stop = False
+
+    pass
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from config import ConfigReader
 import asyncio
 import server.TCPServer
 import server.UDPServer
-
+import dev.FCLight as FCLight
 
 # get config object
 config = ConfigReader.get_config()
@@ -13,18 +13,15 @@ tcp_server = server.TCPServer.TCPServer()
 # init udp server
 udp_server = server.UDPServer.UDPServer()
 
+# dev
 
-@asyncio.coroutine
-def pi_service():
-    while True:
-        print("pi_service")
-        yield from asyncio.sleep(10)
-
+# light
+light = FCLight.FCLight()
 
 # start event loop
 event_loop = asyncio.get_event_loop()
 udp_server.start_server(event_loop)
-tasks = [tcp_server.start_server(), pi_service()]
+tasks = [light.start_service(), light.start_service()]
 event_loop.run_until_complete(asyncio.wait(tasks))
 try:
     event_loop.run_forever()
@@ -32,3 +29,4 @@ except KeyboardInterrupt:
     pass
 finally:
     event_loop.close()
+    light.stop_service()
