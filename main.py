275c91899079f4e983b576bc880eff76d7f5f1f2
zhouxi's_pi <zhouxi121@live.com>
2017-10-16 19:21:04 +0800

    add light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from config import ConfigReader
 import asyncio
-import server.TCPServer
+import server.TCPServer as TCPServer
 import server.UDPServer
 import dev.FCLight as FCLight
 
@@ -8,7 +8,7 @@ import dev.FCLight as FCLight
 config = ConfigReader.get_config()
 
 # init tcp server
-tcp_server = server.TCPServer.TCPServer()
+tcp_server = TCPServer.TCPServer()
 
 # init udp server
 udp_server = server.UDPServer.UDPServer()
@@ -21,7 +21,7 @@ light = FCLight.FCLight()
 # start event loop
 event_loop = asyncio.get_event_loop()
 udp_server.start_server(event_loop)
-tasks = [light.start_service(), light.start_service()]
+tasks = [tcp_server.start_server(), light.start_service()]
 event_loop.run_until_complete(asyncio.wait(tasks))
 try:
     event_loop.run_forever()
